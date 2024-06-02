ee824b0ac753382779d44fe6c7d0738e4d5ff0e6
MakarenkoDaria <daria.naum@mail.ru>
2024-06-02 08:51:19 +0300

    Update lesson_6_task_1.sql
    Исправила модуль 15.4 задание 1. Фамилии в строку, направление в столбце.

--- SQL/sp/lesson_6_task_1.sql
+++ SQL/sp/lesson_6_task_1.sql
@@ -7,13 +7,16 @@ BEGIN
 	IF NOT EXISTS (SELECT 1 FROM Direction WHERE code_id = @code_id) 
 	RAISERROR('Некорректный код направления', 16, 1);
 
-    SELECT s.Name+ ' ' + s.Surname AS Student_name, d.Name_direction
+    SELECT STRING_AGG (s.Name+ ' ' + s.Surname, ', ') AS Student_name, d.Name_direction
     FROM dbo.Students s 
 	join dbo.Direction d
 	on s.Code_id = d.Code_id
 	and s.Exam_score > d.Min_score
 	and s.Code_id = @code_id
+	GROUP BY d.Name_direction
 END;
+
+
 
 
 --exec SP_direction_students 1
